53cadedbc6a88db45385742f36bb0f09855f0ebd
Ghulam Mustafa Zaka <g.mustafa.zaka@gmail.com>
2022-06-28 10:49:28 -0700

    adding comment and edit-post pages

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -2,13 +2,15 @@ async function signupFormHandler(event) {
   event.preventDefault();
 
   const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
-  if (username && password) {
-    const response = await fetch("/api/users/signup", {
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
       method: "post",
       body: JSON.stringify({
         username,
+        email,
         password,
       }),
       headers: { "Content-Type": "application/json" },
@@ -21,6 +23,7 @@ async function signupFormHandler(event) {
     }
   }
 }
+
 document
   .querySelector(".signup-form")
   .addEventListener("submit", signupFormHandler);
