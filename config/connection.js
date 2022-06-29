6ae6f0c0bec730ecff3318ffc53c5d0f9cb83a79
Hector Romo <hectorommmo@gmail.com>
2022-06-28 20:38:14 -0700

    localhost revert

--- config/connection.js
+++ config/connection.js
@@ -9,9 +9,8 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: "127.0.0.1",
+    process.env.DB_PW, {
+      host: "localhost",
       dialect: "mysql",
       port: 3306,
     }
