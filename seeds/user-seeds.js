f6f7b5ae928bb7616d206ce2b880eaa2a95b85d9
Macy Kay Mannix <macymannix@gmail.com>
2022-06-30 18:04:54 -0700

    seeds data

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,21 +1,67 @@
+
 const sequelize = require("../config/connection");
 const { User, Post } = require("../models");
 
 const userdata = [
   {
-    username: "alesmonde0",
-    email: "nwestnedge0@cbc.ca",
-    password: "password123",
+    username: "gmzaka",
+    email: "gmz@apple.com",
+    password: "abc123",
   },
   {
-    username: "jwilloughway1",
-    email: "rmebes1@sogou.com",
-    password: "password123",
+    username: "jboo",
+    email: "jeralyn@drax.com",
+    password: "abc123",
   },
   {
-    username: "iboddam2",
-    email: "cstoneman2@last.fm",
-    password: "password123",
+    username: "heezy",
+    email: "hector@peanut.org",
+    password: "abc123",
+  },
+  {
+    username: "macykcool",
+    email: "mkcool@apple.com",
+    password: "abc123",
+  },
+  {
+    username: "jester",
+    email: "beard@marvel.com",
+    password: "abc123",
+  },
+  {
+    username: "divad",
+    email: "david@gmail.com",
+    password: "abc123",
+  },
+  {
+    username: "skoginthemachine",
+    email: "ryskog@apple.com",
+    password: "abc123",
+  },
+  {
+    username: "rocky",
+    email: "phil@chicago.com",
+    password: "abc123",
+  },
+  {
+    username: "bajaman",
+    email: "who@letemout.org",
+    password: "abc123",
+  },
+  {
+    username: "plankton",
+    email: "pine@apple.com",
+    password: "abc123",
+  },
+  {
+    username: "gritty",
+    email: "flyers@kind.com",
+    password: "abc123",
+  },
+  {
+    username: "dishoffender#1",
+    email: "analiza@peanut.org",
+    password: "abc123",
   },
 ];
 
