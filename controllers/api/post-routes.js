cca72ef8df9436a8190d0d45a5e41f368c931c03
Macy Kay Mannix <macymannix@gmail.com>
2022-07-09 18:41:21 -0700

    controllers

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,12 +1,6 @@
 const router = require("express").Router();
-
-const upload = require("../../utils/upload");
-const uploadController = require("../upload-routes");
-
 const sequelize = require("../../config/connection");
-
 const { Post, User, Comment, Vote } = require("../../models");
-
 const withAuth = require("../../utils/auth");
 
 // get all users
@@ -94,16 +88,13 @@ router.get("/:id", (req, res) => {
     });
 });
 
-
 router.post("/", withAuth, (req, res) => {
   // expects {title: 'Taskmaster goes public!', content: 'https://taskmaster.com/press', user_id: 1}
-
   Post.create({
     title: req.body.title,
     content: req.body.content,
     data: req.body.data,
     user_id: req.session.user_id,
-    data: req.session.data,
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -170,3 +161,180 @@ router.delete("/:id", withAuth, (req, res) => {
 });
 
 module.exports = router;
+
+
+
+//delete
+
+// const router = require("express").Router();
+
+// const upload = require("../../utils/upload");
+// const uploadController = require("../upload-routes");
+
+// const sequelize = require("../../config/connection");
+
+// const { Post, User, Comment, Vote } = require("../../models");
+
+// const withAuth = require("../../utils/auth");
+
+// // get all users
+// router.get("/", (req, res) => {
+//   console.log("======================");
+//   Post.findAll({
+//     attributes: [
+//       "id",
+//       "content",
+//       "title",
+//       "data",
+//       "created_at",
+//       [
+//         sequelize.literal(
+//           "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+//         ),
+//         "vote_count",
+//       ],
+//     ],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ["username"],
+//       },
+//     ],
+//   })
+//     .then((dbPostData) => res.json(dbPostData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// router.get("/:id", (req, res) => {
+//   Post.findOne({
+//     where: {
+//       id: req.params.id,
+//     },
+//     attributes: [
+//       "id",
+//       "content",
+//       "title",
+//       "data",
+//       "created_at",
+//       [
+//         sequelize.literal(
+//           "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+//         ),
+//         "vote_count",
+//       ],
+//     ],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ["username"],
+//       },
+//     ],
+//   })
+//     .then((dbPostData) => {
+//       if (!dbPostData) {
+//         res.status(404).json({ message: "No post found with this id" });
+//         return;
+//       }
+//       res.json(dbPostData);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+
+// router.post("/", withAuth, (req, res) => {
+//   // expects {title: 'Taskmaster goes public!', content: 'https://taskmaster.com/press', user_id: 1}
+
+//   Post.create({
+//     title: req.body.title,
+//     content: req.body.content,
+//     data: req.body.data,
+//     user_id: req.session.user_id,
+//     data: req.session.data,
+//   })
+//     .then((dbPostData) => res.json(dbPostData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// router.put("/upvote", withAuth, (req, res) => {
+//   // custom static method created in models/Post.js
+//   Post.upvote(
+//     { ...req.body, user_id: req.session.user_id },
+//     { Vote, Comment, User }
+//   )
+//     .then((updatedVoteData) => res.json(updatedVoteData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// router.put("/:id", withAuth, (req, res) => {
+//   Post.update(
+//     {
+//       title: req.body.title,
+//     },
+//     {
+//       where: {
+//         id: req.params.id,
+//       },
+//     }
+//   )
+//     .then((dbPostData) => {
+//       if (!dbPostData) {
+//         res.status(404).json({ message: "No post found with this id" });
+//         return;
+//       }
+//       res.json(dbPostData);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// router.delete("/:id", withAuth, (req, res) => {
+//   console.log("id", req.params.id);
+//   Post.destroy({
+//     where: {
+//       id: req.params.id,
+//     },
+//   })
+//     .then((dbPostData) => {
+//       if (!dbPostData) {
+//         res.status(404).json({ message: "No post found with this id" });
+//         return;
+//       }
+//       res.json(dbPostData);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,13 +1,10 @@
 const router = require("express").Router();
 
 const sequelize = require("../config/connection");
-
 const { Post, User, Comment, Vote, Image } = require("../models");
-
 
 // const multer = require("multer");
 // const upload = multer({ dest: "/public/uploads/" });
-
 
 // get all posts for homepage
 router.get("/", (req, res) => {
@@ -126,11 +123,145 @@ router.get("/new-post", (req, res) => {
   res.render("new-post");
 });
 
-router.get('/login', (req, res) => {
-   res.render('login');
-});
-
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard');
-});
 module.exports = router;
+
+
+// delete 
+
+
+// const router = require("express").Router();
+
+// const sequelize = require("../config/connection");
+
+// const { Post, User, Comment, Vote, Image } = require("../models");
+
+
+// // const multer = require("multer");
+// // const upload = multer({ dest: "/public/uploads/" });
+
+
+// // get all posts for homepage
+// router.get("/", (req, res) => {
+//   console.log("======================");
+//   Post.findAll({
+//     attributes: [
+//       "id",
+//       "content",
+//       "title",
+//       "data",
+//       "created_at",
+//       [
+//         sequelize.literal(
+//           "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+//         ),
+//         "vote_count",
+//       ],
+//     ],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ["username"],
+//       },
+//     ],
+//   })
+//     .then((dbPostData) => {
+//       const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+//       res.render("homepage", {
+//         posts,
+//         loggedIn: req.session.loggedIn,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// // get single post
+// router.get("/post/:id", (req, res) => {
+//   Post.findOne({
+//     where: {
+//       id: req.params.id,
+//     },
+//     attributes: [
+//       "id",
+//       "content",
+//       "title",
+//       "data",
+//       "created_at",
+//       [
+//         sequelize.literal(
+//           "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+//         ),
+//         "vote_count",
+//       ],
+//     ],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ["username"],
+//       },
+//     ],
+//   })
+//     .then((dbPostData) => {
+//       if (!dbPostData) {
+//         res.status(404).json({ message: "No post found with this id" });
+//         return;
+//       }
+
+//       const post = dbPostData.get({ plain: true });
+
+//       res.render("single-post", {
+//         post,
+//         loggedIn: req.session.loggedIn,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// router.get("/login", (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect("/");
+//     return;
+//   }
+
+//   res.render("login");
+// });
+
+// // display signup page
+// router.get("/signup", (req, res) => {
+//   res.render("signup");
+// });
+
+// router.get("/new-post", (req, res) => {
+//   res.render("new-post");
+// });
+
+// router.get('/login', (req, res) => {
+//    res.render('login');
+// });
+
+// router.get('/dashboard', (req, res) => {
+//   res.render('dashboard');
+// });
+// module.exports = router;
