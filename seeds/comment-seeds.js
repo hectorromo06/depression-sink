40bfb4cb65fbfa77e5c4a82ea810ecb8a23e2c8e
Macy Kay Mannix <macymannix@gmail.com>
2022-07-09 19:40:33 -0700

    bring back sink seeds

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -7,6 +7,145 @@ const commentdata = [
     user_id: 2,
     post_id: 1,
   },
+  {
+    comment_text:
+      "i seen worse",
+    user_id: 4,
+    post_id: 1,
+  },
+  {
+    comment_text: "maybe $50",
+    user_id: 3,
+    post_id: 2,
+  },
+  {
+    comment_text: "aren't we all",
+    user_id: 1,
+    post_id: 3,
+  },
+  {
+    comment_text:
+      "musty",
+    user_id: 2,
+    post_id: 4,
+  },
+  {
+    comment_text: "yikes!!",
+    user_id: 3,
+    post_id: 5,
+  },
+  {
+    comment_text: "WAY TO GO!!",
+    user_id: 10,
+    post_id: 7,
+  },
+  {
+    comment_text:
+      "teach me you ways",
+    user_id: 9,
+    post_id: 7,
+  },
+  {
+    comment_text: "facts",
+    user_id: 8,
+    post_id: 6,
+  },
+  {
+    comment_text: "hurricane",
+    user_id: 1,
+    post_id: 8,
+  },
+  {
+    comment_text:
+      "yolo",
+    user_id: 9,
+    post_id: 9,
+  },
+  {
+    comment_text: "life is so unfair",
+    user_id: 8,
+    post_id: 10,
+  },
+  {
+    comment_text: "dramatic",
+    user_id: 7,
+    post_id: 11,
+  },
+  {
+    comment_text:
+      "roomates are a scam!!",
+    user_id: 2,
+    post_id: 5,
+  },
+  {
+    comment_text: "what's it like to be perfect?",
+    user_id: 9,
+    post_id: 7,
+  },
+  {
+    comment_text: "whats the meaning of life?",
+    user_id: 3,
+    post_id: 6,
+  },
+  {
+    comment_text:
+      "daddy gang",
+    user_id: 2,
+    post_id: 13,
+  },
+  {
+    comment_text: "ikyfl.",
+    user_id: 4,
+    post_id: 13,
+  },
+  {
+    comment_text: "IKDR",
+    user_id: 4,
+    post_id: 14,
+  },
+  {
+    comment_text:
+      "welcome home",
+    user_id: 2,
+    post_id: 15,
+  },
+  {
+    comment_text: "its giving, patriarchy",
+    user_id: 3,
+    post_id: 13,
+  },
+  {
+    comment_text: "ITS GIVING GIRL BOSS",
+    user_id: 1,
+    post_id: 14,
+  },
+  {
+    comment_text:
+      "with all due respect, he's the founding father of the scrub empire",
+    user_id: 5,
+    post_id: 13,
+  },
+  {
+    comment_text: "MOMMY SUPREMECY",
+    user_id: 2,
+    post_id: 14,
+  },
+  {
+    comment_text: "abe lincoln of sponges",
+    user_id: 10,
+    post_id: 13,
+  },
+  {
+    comment_text:
+      "Spongebob is my founding father",
+    user_id: 4,
+    post_id: 13,
+  },
+  {
+    comment_text: "Patrick, Spongebob, Gary and Sandy for mt rushmore 2024",
+    user_id: 10,
+    post_id: 13,
+  },
 ];
 
 const seedComments = () => Comment.bulkCreate(commentdata);
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,11 +2,98 @@ const { Post } = require("../models");
 
 const postdata = [
   {
-
-    title: "Donec posuere metus vitae ipsum.",
-    content: "this is a good idea.",
-    data: "https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png",
+    title: "CANT DO THIS",
+    image: "../resources/static/assets/uploads/one.jpg",
     user_id: 1,
+  },
+  {
+    title: "Could I pay you 20 bucks to do these?",
+    image: "../resources/static/assets/uploads/two.jpg",
+    user_id: 2,
+  },
+  {
+    title:
+      "Doomed",
+      image: "../resources/static/assets/uploads/three.jpg",
+    user_id: 3,
+  },
+  {
+    title:
+      "been brewing for a week",
+      image: "../resources/static/assets/uploads/four.jpg",
+    user_id: 4,
+  },
+  {
+    title:
+      "i hate my roommate",
+      image: "../resources/static/assets/uploads/five.jpg",
+    user_id: 5,
+  },
+  {
+    title:
+      "i guess we have to do dishes until we die, rip off",
+      image: "../resources/static/assets/uploads/six.jpg",
+    user_id: 3,
+  },
+  {
+    title:
+      "CLEAN!!",
+      image: "../resources/static/assets/uploads/seven.jpg",
+    user_id: 6,
+  },
+  {
+    title:
+      "bender",
+      image: "../resources/static/assets/uploads/eight.jpg",
+    user_id: 10,
+  },
+  {
+    title:
+      "dgaf!",
+      image: "../resources/static/assets/uploads/nine.jpg",
+    user_id: 7,
+  },
+  {
+    title:
+      "can someone just",
+      image: "../resources/static/assets/uploads/ten.jpg",
+    user_id: 5,
+  },
+  {
+    title:
+      "crying cuz people have dish washers",
+      image: "../resources/static/assets/uploads/eleven.jpg",
+    user_id: 4,
+  },
+  {
+    title:
+      "nightmare",
+      image: "../resources/static/assets/uploads/twelve.jpg",
+    user_id: 8,
+  },
+  {
+    title:
+      "scrub daddys only",
+      image: "../resources/static/assets/uploads/thirteen.jpg",
+    user_id: 7,
+  },
+  {
+    title:
+      "THIS IS A SCRUB MOMMY HOUSEHOLD",
+      image: "../resources/static/assets/uploads/fourteen.jpg",
+    user_id: 5,
+  },
+  {
+    title:
+      "i give up",
+      image: "../resources/static/assets/uploads/fifteen.jpg",
+    user_id: 4,
+  },
+  {
+    title:
+      "bye",
+      image: "../resources/static/assets/uploads/sixteen.jpg",
+    user_id: 8,
   },
 
 ];
