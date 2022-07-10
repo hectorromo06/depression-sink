66c93402ec63470960160808e155328ccd4ca3b5
Macy Kay Mannix <macymannix@gmail.com>
2022-07-10 14:58:06 -0700

    trying to get front end back

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const sequelize = require("../config/connection");
-const { Post, User, Comment, Vote} = require("../models");
+const { Post, User, Comment, Vote } = require("../models");
 
 //deleted Image for now
 
