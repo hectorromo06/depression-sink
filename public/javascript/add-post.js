2f9f13a716dcb2506c3b3003414f8850098e1a19
Macy Kay Mannix <macymannix@gmail.com>
2022-07-09 19:34:17 -0700

    java

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,10 +2,21 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-
   const content = document.querySelector('input[name="post-content"]').value;
   const data = document.querySelector('input[name="file"]').value;
-
+  //const data = document.querySelector('input[name="file"]').files[0].name;
+  //const data = document.getElementById("input-files").files[0].name;
+  // const data = document.getElementById("inputFile");
+  // const fReader = new FileReader();
+  // fReader.readAsDataURL(input.files[0]);
+  // fReader.onloadend = function (event) {
+  //   var img = document.getElementById("inputFile");
+  //   img.src = event.target.result;
+  // };
+  // const data = (document.getElementById("input-files").onchange = function () {
+  //   document.getElementById("input-files").value = this.value;
+  //   document.getElementById("input-files").files[0].fileName;
+  // });
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
@@ -29,3 +40,35 @@ async function newFormHandler(event) {
 document
   .querySelector(".new-post-form")
   .addEventListener("submit", newFormHandler);
+
+// async function newFormHandler(event) {
+//   event.preventDefault();
+
+//   const title = document.querySelector('input[name="post-title"]').value;
+
+//   const content = document.querySelector('input[name="post-content"]').value;
+//   const data = document.querySelector('input[name="file"]').value;
+
+
+//   const response = await fetch(`/api/posts`, {
+//     method: "POST",
+//     body: JSON.stringify({
+//       title,
+//       content,
+//       data,
+//     }),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+
+//   if (response.ok) {
+//     document.location.replace("/dashboard");
+//   } else {
+//     alert(response.statusText);
+//   }
+// }
+
+// document
+//   .querySelector(".new-post-form")
+//   .addEventListener("submit", newFormHandler);
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,5 @@
+
+
 async function loginFormHandler(event) {
   event.preventDefault();
 
@@ -25,3 +27,31 @@ async function loginFormHandler(event) {
 document
   .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
+
+// async function loginFormHandler(event) {
+//   event.preventDefault();
+
+//   const email = document.querySelector("#email-login").value.trim();
+//   const password = document.querySelector("#password-login").value.trim();
+
+//   if (email && password) {
+//     const response = await fetch("/api/users/login", {
+//       method: "post",
+//       body: JSON.stringify({
+//         email,
+//         password,
+//       }),
+//       headers: { "Content-Type": "application/json" },
+//     });
+
+//     if (response.ok) {
+//       document.location.replace("/");
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+// }
+
+// document
+//   .querySelector(".login-form")
+//   .addEventListener("submit", loginFormHandler);
